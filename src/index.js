36f4977c45120906e72e22af38ce4e58b200356c
Gomolemo Bojosi <gee@geebojosi.com>
2021-05-16 22:54:05 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -6,12 +6,3 @@ init();
 function init() {
   loadPage();
 }
-// const content = document.querySelector('#content');
-
-// const link = document.createElement('a');
-// // link.setAttribute('href', loadPage);
-// link.textContent = 'Click here';
-
-// link.addEventListener('click', loadPage); 
-
-// content.appendChild(link)
